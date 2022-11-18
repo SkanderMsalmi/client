f051bac7820a91891502127c016c0c28fcbaaad5
Skander <msalmi.skander@esprit.tn>
2022-11-18 15:08:59 +0100

    create routing / module (for lazyLoading)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'dashboard',component:DashboardComponent},
+  { path: 'etudiants', loadChildren: () => import('./etudiant/etudiant.module').then(m => m.EtudiantModule) },
+ { path: 'equipes', loadChildren: () => import('./equipe/equipe.module').then(m => m.EquipeModule) },
+  { path: 'contrats', loadChildren: () => import('./contrat/contrat.module').then(m => m.ContratModule) },
+   { path: 'universites', loadChildren: () => import('./universite/universite.module').then(m => m.UniversiteModule) },
+ { path: 'departements', loadChildren: () => import('./departement/departement.module').then(m => m.DepartementModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { AsideComponent } from './shared/aside/aside.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HomeComponent,
+    NotFoundComponent,
+    NavbarComponent,
+    AsideComponent,
+    DashboardComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
